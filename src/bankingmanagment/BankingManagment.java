e213c5e1a1ea6474d4c93fe7c547c1a559f51c21
Swalihvk17 <swalihshalusr@gmail.com>
2020-02-26 11:51:06 +0530

    project

---
+++ src/bankingmanagment/BankingManagment.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankingmanagment;
+
+/**
+ *
+ * @author ssmpoly
+ */
+public class BankingManagment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HomePage pp = new HomePage();
+        pp.setVisible(true);
+    }
+    
+}
